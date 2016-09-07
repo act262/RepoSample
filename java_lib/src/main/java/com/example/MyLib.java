89ab1dedde1d246935921ae4616048ef291bd25b
act262 <act262@gmail.com>
2016-09-07 18:55:40 +0800

    add java lib sample

---
+++ java_lib/src/main/java/com/example/MyLib.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * MyLib Class
+ *
+ * @author act262@gmail.com
+ */
+public class MyLib {
+    public static void show() {
+        System.out.println("MyLib.show");
+    }
+}
