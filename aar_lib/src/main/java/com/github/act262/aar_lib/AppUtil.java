03ab5581f800ec9ae575fd700ea49e33f7dae67e
act262 <act262@gmail.com>
2016-09-19 16:02:55 +0800

    add aar library sample

---
+++ aar_lib/src/main/java/com/github/act262/aar_lib/AppUtil.java
@@ -0,0 +1,13 @@
+package com.github.act262.aar_lib;
+
+import android.util.Log;
+
+/**
+ * App Utility
+ */
+public class AppUtil {
+
+    public static void show() {
+        Log.d("AppUtil", "show: hello lib");
+    }
+}
