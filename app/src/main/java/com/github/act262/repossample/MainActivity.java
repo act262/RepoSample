739c8a3497dbead7305dfccfde40e06144506e17
act262 <act262@gmail.com>
2016-09-19 16:22:10 +0800

    add app lib sample dependencies

--- app/src/main/java/com/github/act262/repossample/MainActivity.java
+++ app/src/main/java/com/github/act262/repossample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.github.act262.repossample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.MyLib;
+import com.github.act262.aar_lib.AppUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // java lib
+        MyLib.show();
+
+        // android lib
+        AppUtil.show();
     }
 }
