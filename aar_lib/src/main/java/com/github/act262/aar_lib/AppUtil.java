d46a709974ac14a048d0963ac60ed427aff28b3b
act262 <act262@gmail.com>
2016-09-28 01:13:41 +0800

    update artifact

--- aar_lib/src/main/java/com/github/act262/aar_lib/AppUtil.java
+++ aar_lib/src/main/java/com/github/act262/aar_lib/AppUtil.java
@@ -1,6 +1,8 @@
 package com.github.act262.aar_lib;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * App Utility
@@ -10,4 +12,13 @@ public class AppUtil {
     public static void show() {
         Log.d("AppUtil", "show: hello lib");
     }
+
+    public static void showToast(Context context, CharSequence sequence) {
+        Toast.makeText(context, sequence, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showToast(Context context, String string) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).show();
+    }
+
 }
